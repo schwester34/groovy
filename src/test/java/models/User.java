adc12ef0561e9fb2e6d462e23073d327139ad3eb
schwester34 <99205353+schwester34@users.noreply.github.com>
2022-07-01 20:08:12 +0500

    Update User.java

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -4,17 +4,19 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
 
-    @Data
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    public class User {
-        private Integer id;
+    @JsonProperty("id")
+    private Integer id;
 
-        private String email;
+    @JsonProperty("email")
+    private String email;
 
-        @JsonProperty("first_name")
-        private String firstName;
+    @JsonProperty("first_name")
+    private String firstName;
 
-        @JsonProperty("last_name")
-        private String lastName;
+    @JsonProperty("last_name")
+    private String lastName;
 }
